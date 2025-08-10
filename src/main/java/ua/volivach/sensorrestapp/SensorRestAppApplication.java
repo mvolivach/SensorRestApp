1f0f79905242244d57c47bedef96ca67bbee908e
mvolivach <mvolivach2003@gmail.com>
2025-08-10 11:48:20 +0300

    first commit

---
+++ src/main/java/ua/volivach/sensorrestapp/SensorRestAppApplication.java
@@ -0,0 +1,13 @@
+package ua.volivach.sensorrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorRestAppApplication.class, args);
+    }
+
+}
